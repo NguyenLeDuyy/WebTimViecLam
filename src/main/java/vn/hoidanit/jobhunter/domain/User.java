6300d0beb42473508bc8a693a65aa18650389376
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-06-10 22:05:40 +0700

    update user model

--- src/main/java/vn/hoidanit/jobhunter/domain/User.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/User.java
@@ -1,13 +1,16 @@
 package vn.hoidanit.jobhunter.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import vn.hoidanit.jobhunter.util.constant.GenderEnum;
+
+import java.time.Instant;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -17,36 +20,15 @@ public class User {
     private String email;
     private String password;
 
-    public String getName() {
-        return name;
-    }
+    private int age;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    private String address;
+    private String refreshToken;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String createdBy;
+    private String updateBy;
 }
